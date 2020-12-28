2e1cda0ec96820a770f76fb8e4c87a4532c7a20a
unknown <daniellopezj0327@gmail.com>
2020-12-28 16:04:52 -0500

    changes

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,40 +1,44 @@
-import React from 'react';
-import Header from '../components/Header';
-import Search from '../components/Search';
-import Categories from '../components/Categories';
-import Carousel from '../components/Carousel';
-import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
-import useInitialState from '../hooks/useInitialState';
-import '../assets/styles/App.scss';
+import React from "react";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import useInitialState from "../hooks/useInitialState";
+import "../assets/styles/App.scss";
 
-const API = 'http://localhost:3000/initalState';
+const API = "http://localhost:3000/initalState";
 
 const Home = () => {
   const initialState = useInitialState(API);
-  return initialState.length === 0 ? <h1>Loading...</h1> : (
-    <div className='App'>
-      <Header />
+  return initialState.length === 0 ? (
+    <h1>Loading...</h1>
+  ) : (
+    <>
       <Search />
       {initialState.mylist.length > 0 && (
-        <Categories title='Mi Lista'>
+        <Categories title="Mi Lista">
           <Carousel>
-            {initialState.mylist.map(item => <CarouselItem key={item.id} {...item} />)}
+            {initialState.mylist.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
           </Carousel>
         </Categories>
       )}
-      <Categories title='Tendencias'>
+      <Categories title="Tendencias">
         <Carousel>
-          {initialState.trends.map(item => <CarouselItem key={item.id} {...item} />)}
+          {initialState.trends.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
         </Carousel>
       </Categories>
-      <Categories title='Originales de Platzi Video'>
+      <Categories title="Originales de Platzi Video">
         <Carousel>
-          {initialState.originals.map(item => <CarouselItem key={item.id} {...item} />)}
+          {initialState.originals.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
         </Carousel>
       </Categories>
-      <Footer />
-    </div>
+    </>
   );
 };
 
