860fada80294f26167deea1395ab65c9e020bd6e
unknown <daniellopezj0327@gmail.com>
2020-12-29 14:23:56 -0500

    changes...

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,7 +12,7 @@ const Home = ({ myList, trends, originals }) => {
     <>
       <Header />
 
-      <Search />
+      <Search isHome />
       {myList.length > 0 && (
         <Categories title="Mi Lista">
           <Carousel>
